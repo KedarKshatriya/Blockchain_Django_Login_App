9e531d5f910a0fcbed48c42548294169d358113e
Kedar Kshatriya <kshatriyakedar@gmail.com>
2020-07-14 19:01:19 +0530

    Integration with database

--- dblogin/dbapp/views.py
+++ dblogin/dbapp/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import  userlogin
 
-# Create your views here.
+
 def loginbl(request):
     return render(request,'dbapp/login.html')
 
 def registerbl(request):
+    if request.method == "POST":
+        username = request.POST.get("usern")
+        address = request.POST.get("address")
+        users = userlogin.filter(username=username)
+        print("Users: ",users)
+
+        return HttpResponse("Check the shit first")
     return render(request,'dbapp/register.html')
 
+def registercheck(request):
+    return HttpResponse("False")
 
 def forgetbl(request):
     return render(request,'dbapp/forget.html')
